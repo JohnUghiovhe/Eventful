f530b160620c260da8eba7e7edcc924ed58b6151
John Ughiovhe <johnandrewjaw@gmail.com>
2026-02-09 21:47:00 +0100

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: {
+          50: '#faf7f2',
+          100: '#f5f3f0',
+          light: '#faf7f2'
+        },
+        navDark: '#2d3748'
+      }
+    },
   },
   plugins: [],
 }
