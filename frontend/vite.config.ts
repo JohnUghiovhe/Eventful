2c629cbb29f3efa4379c8660a761d07bd52f43e9
John Ughiovhe <johnandrewjaw@gmail.com>
2026-02-09 14:29:09 +0100

    Add files via upload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,33 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
   server: {
-    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+        target: 'https://eventful-api.onrender.com',
+        changeOrigin: true
+      }
+    }
+  }
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
